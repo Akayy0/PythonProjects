4a314ab535ff6a4ad019dcf68e164516ef5ac1c6
João Vitor <joaovitorotoni6@gmail.com>
2022-11-10 10:55:20 -0300

    PY3 activity complete

---
+++ PY-Training-Activities/PY3.py
@@ -0,0 +1,11 @@
+# You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+# Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+# For example, when the input is green, output should be yellow.
+
+def update_light(current):
+    if current == 'green':
+        return ("yellow")
+    elif current == 'yellow':
+        return ("red")
+    elif current == 'red':
+        return("green")
